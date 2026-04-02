ffafe381b88c2a5ce7aae0aeb5d6605e3901eae6
Lin_Hou_yu <zhishi_fenzi@hotmail.com>
2026-04-02 03:33:50 -0300

    updata
    transform change addforce

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,34 +4,48 @@ public class PlayerMove : MonoBehaviour
 {
     public float sideSpeed = 10f;    //左右躲闪的速度
     public float xBoundary = 4.5f;   //左右边界
-    public float jumpforce = 5f;
-    public bool isGrounded;
+    public float jumpForce = 5f;     //跳跃的力度
+    public bool isGrounded;          
+
+    private Rigidbody _rb;            
+
+    void Start()
+    {
+       
+        _rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
-        //获取玩家的左右按键输入
-        var horizontalInput = Input.GetAxis("Horizontal");
-        Input.GetAxis("Vertical");
+   
+        float horizontalInput = Input.GetAxis("Horizontal");
         
+
         transform.Translate(Vector3.right * (horizontalInput * sideSpeed * Time.deltaTime));
 
-        
-        var currentPosition = transform.position;
+
+        Vector3 currentPosition = transform.position;
         currentPosition.x = Mathf.Clamp(currentPosition.x, -xBoundary, xBoundary);
         transform.position = currentPosition;
 
-        if (!Input.GetButtonDown("Jump")) return;
-        transform.Translate(Vector3.up * (jumpforce * Time.deltaTime));
+       
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            
+            
+            isGrounded = false;
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+        if (collision.gameObject.CompareTag("isGrounded"))
+        {
+            isGrounded = true;
+        }
     }
 }
